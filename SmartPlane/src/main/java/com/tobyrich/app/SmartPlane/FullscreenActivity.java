776013f820df8bb78477a5f5e0a6722e6c8b60e5
Samit Vaidya <samit.vaidya@tobyrich.com>
2014-06-10 16:30:12 +0200

    refactored code for powerup
    Summary: added Smartplane folder
    
    Test Plan: both power up and smartplane can be installed
    
    Reviewers: prashant.vaibhav
    
    Differential Revision: https://office.tobyrich.com/D44

---
+++ SmartPlane/src/main/java/com/tobyrich/app/SmartPlane/FullscreenActivity.java
@@ -0,0 +1,155 @@
+package com.tobyrich.app.SmartPlane;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.tobyrich.app.SmartPlane.util.Const;
+import com.tobyrich.app.SmartPlane.util.InfoBox;
+import com.tobyrich.app.SmartPlane.util.Util;
+import com.tailortoys.app.PowerUp.R;
+
+import lib.smartlink.BluetoothDevice;
+
+/**
+ * @author Samit Vaidya
+ * @date 04 March 2014
+ * Refactored by: Radu Hambasan
+ */
+
+public class FullscreenActivity extends Activity {
+    private static final String TAG = "SmartPlane";
+
+    private BluetoothDelegate bluetoothDelegate;  // bluetooth events
+    private SensorHandler sensorHandler;  // accelerometer & magnetometer
+    private GestureDetector gestureDetector;  // touch events
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // The resolution might have changed while the app was paused
+        ViewTreeObserver viewTree = findViewById(R.id.controlPanel).getViewTreeObserver();
+        viewTree.addOnGlobalLayoutListener(new GlobalLayoutListener(this));
+
+        sensorHandler.registerListener();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorHandler.unregisterListener();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+
+        Util.inform(this, "Pull Up to Start the Motor");
+
+        String appVersion = "";
+        try {
+            appVersion = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.w(TAG, "Could not locate package; needed to set appVersion.");
+            e.printStackTrace();
+        }
+
+        ImageView infoButton = (ImageView) findViewById(R.id.imgInfo);
+        InfoBox infoBox = new InfoBox(Const.UNKNOWN, infoButton, appVersion);
+        infoButton.setOnClickListener(infoBox);
+
+        bluetoothDelegate = new BluetoothDelegate(this, infoBox);
+        sensorHandler = new SensorHandler(this, bluetoothDelegate);
+        sensorHandler.registerListener();
+        gestureDetector = new GestureDetector(this,
+                new GestureListener(this, bluetoothDelegate));
+
+         /* setting the trivial listeners */
+        ImageView horizonImage = (ImageView) findViewById(R.id.imageHorizon);
+        horizonImage.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        });
+
+        // sound played when the user presses the "Control Tower" button
+        final MediaPlayer atcSound = MediaPlayer.create(this, R.raw.atc_sounds1);
+
+        final ImageView atcOffButton = (ImageView) findViewById(R.id.atcOff);
+        final ImageView atcOnButton = (ImageView) findViewById(R.id.atcOn);
+
+        atcOffButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atcOnButton.setVisibility(View.VISIBLE);
+                v.setVisibility(View.GONE);
+                atcSound.start();
+            }
+        });
+
+        atcOnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atcOffButton.setVisibility(View.VISIBLE);
+                v.setVisibility(View.GONE);
+                atcSound.pause();
+            }
+        });
+
+        ImageView controlPanel = (ImageView) findViewById(R.id.imgPanel);
+        controlPanel.setOnTouchListener(new PanelTouchListener(this,
+                bluetoothDelegate));
+
+        final ImageView revRudder = (ImageView) findViewById(R.id.revRudder);
+        final Switch rudderSwitch = (Switch) findViewById(R.id.rudderSwitch);
+        final TextView revRudderText = (TextView) findViewById(R.id.revText);
+
+        revRudder.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                revRudder.setVisibility(View.INVISIBLE);
+                rudderSwitch.setVisibility(View.VISIBLE);
+                revRudderText.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        rudderSwitch.setVisibility(View.INVISIBLE);
+                        revRudderText.setVisibility(View.INVISIBLE);
+                        revRudder.setVisibility(View.VISIBLE);
+                    }
+                }, Const.HIDE_REVRUDDER_DELAY);
+                return true;
+            }
+        });
+    }  // End onCreate()
+
+
+    // TODO: why?
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            BluetoothDevice device = bluetoothDelegate.getBluetoothDevice();
+            if (device != null) {
+                device.connect(); // start scanning and connect
+            }
+        } else {
+            Log.e(TAG, "Bluetooth enabling was canceled by user");
+        }
+    }
+}
+
---
+++ SmartPlane/src/main/java/com/tobyrich/app/SmartPlane/GestureListener.java
@@ -0,0 +1,78 @@
+package com.tobyrich.app.SmartPlane;
+
+import android.app.Activity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.tobyrich.app.SmartPlane.util.Const;
+import com.tobyrich.app.SmartPlane.util.Util;
+import com.tailortoys.app.PowerUp.R;
+
+import lib.smartlink.driver.BLESmartplaneService;
+
+/**
+ * @author Samit Vaidya
+ * @date 04 March 2014
+ * Refactored by: Radu Hambasan
+ */
+
+public class GestureListener extends GestureDetector.SimpleOnGestureListener {
+    private Activity activity;
+    private BluetoothDelegate bluetoothDelegate;
+    private boolean tapped;
+
+    public GestureListener(Activity activity, BluetoothDelegate bluetoothDelegate) {
+        this.activity = activity;
+        this.bluetoothDelegate = bluetoothDelegate;
+        tapped = false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        // consume event
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        final ImageView controlPanel = (ImageView) activity.findViewById(R.id.imgPanel);
+        final ImageView throttleLock = (ImageView) activity.findViewById(R.id.lockThrottle);
+        final ImageView slider = (ImageView) activity.findViewById(R.id.throttleCursor);
+        final float currHeight = controlPanel.getHeight();
+        final float newHeight = Const.SCALE_FOR_CURSOR_RANGE * currHeight;
+
+        tapped = !tapped;
+
+        // if double tapped, show the locked image, move the slider to min position
+        // move throttle needle to min, stop ontouch events on slider and controlpanel
+        // and set motor value to 0
+        if (tapped) {
+            ImageView throttleNeedle = (ImageView) activity.findViewById(R.id.imgThrottleNeedle);
+            TextView throttleText = (TextView) activity.findViewById(R.id.throttleValue);
+            throttleLock.setVisibility(View.VISIBLE);
+
+            slider.setY(newHeight);
+            Util.rotateImageView(throttleNeedle, 0, Const.THROTTLE_NEEDLE_MIN_ANGLE,
+                    Const.THROTTLE_NEEDLE_MAX_ANGLE);
+            throttleText.setText("0" + "%");
+
+            // turning the ontouch listener off
+            slider.setEnabled(false);
+            controlPanel.setEnabled(false);
+
+            BLESmartplaneService smartplaneService = bluetoothDelegate.getSmartplaneService();
+            if (smartplaneService != null) {
+                smartplaneService.setMotor((short) 0);
+            }
+        } else {
+            throttleLock.setVisibility(View.INVISIBLE);
+            slider.setEnabled(true);
+            controlPanel.setEnabled(true);
+        }
+
+        return true;
+    }
+}

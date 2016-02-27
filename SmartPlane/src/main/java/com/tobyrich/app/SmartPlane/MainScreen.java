b2fe8b936166e10327d0d47bc7e35beebe079ca1
Joao Mota <joaomrmota28@gmail.com>
2016-02-27 19:17:36 +0000

    just a test

---
+++ SmartPlane/src/main/java/com/tobyrich/app/SmartPlane/MainScreen.java
@@ -0,0 +1,16 @@
+package com.tobyrich.app.SmartPlane;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.tailortoys.app.PowerUp.R;
+
+public class MainScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+
+}

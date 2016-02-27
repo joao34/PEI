ab96189b318ac1f925e50ae4f7fdcefc5b5ede73
Joao Mota <joaomrmota28@gmail.com>
2016-02-27 20:34:01 +0000

    added menu

--- SmartPlane/src/main/java/com/tobyrich/app/SmartPlane/MainScreen.java
+++ SmartPlane/src/main/java/com/tobyrich/app/SmartPlane/MainScreen.java
@@ -1,7 +1,10 @@
 package com.tobyrich.app.SmartPlane;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 import com.tailortoys.app.PowerUp.R;
 
@@ -11,6 +14,26 @@ public class MainScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+
+        Button btn = (Button)findViewById(R.id.bttnsolo);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainScreen.this, FullscreenActivity.class));
+            }
+        });
     }
+
+
+   /* protected void onClickSolo(View v){
+        startActivity(new Intent(MainScreen.this, FullscreenActivity.class));
+    }
+
+    protected void onClickMultiplayer(View v){
+
+    }
+    */
 
 }

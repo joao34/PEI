cfa01d125349a0965fe006f773cbdaf18d866a46
Joao Mota <joaomrmota28@gmail.com>
2016-02-27 21:35:39 +0000

    some changes

---
+++ SmartPlane/src/main/java/com/tobyrich/app/SmartPlane/InfoActivity.java
@@ -0,0 +1,23 @@
+package com.tobyrich.app.SmartPlane;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+import com.tailortoys.app.PowerUp.R;
+
+public class InfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+
+
+
+    }
+
+}
